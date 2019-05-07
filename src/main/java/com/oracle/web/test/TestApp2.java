c0042d0cad96c379c68921bc3056e39bf8dbfa1a
zhangxin <2301410761@qq.com>
2019-05-07 11:18:14 +0800

    添加一个TestApp2类

---
+++ src/main/java/com/oracle/web/test/TestApp2.java
@@ -0,0 +1,10 @@
+package com.oracle.web.test;
+
+public class TestApp2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ System.out.println("left........");
+	}
+
+}
